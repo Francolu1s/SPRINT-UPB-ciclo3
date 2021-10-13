0c33d0e8e78d073cac8066dab2c6cd79e3632b27
Francolu1s <franco.luis@ive..com>
2021-10-13 10:23:57 -0500

    ActualizacionSprint2verisionWilliam

--- Sprint2/validaciones.js
+++ Sprint2/validaciones.js
@@ -1,52 +1,61 @@
-const usuario =document.querySelector("#in_nombre_usuario");
-const stringA =document.querySelector("#in_contrasena");
-const stringB =document.querySelector("#in_confirmar_contrasena");
-const boton_registro =document.querySelector("#btn_registrar_usuario");
-
-console.log(usuario.value);
-
-boton_registro.addEventListener("click", validar_nombre_usuario, validar_nombre_usuario, validar_contrasena, confirmar_contrasena);
-
-
-function validar_nombre_usuario(usuario){
-    if(usuario.value.length>=6 && usuario.value.length<=30){
+function validar_nombre_usuario(string){
+    // La ñ arroja falso no esta incluida
+    patron2 = /^[a-zA-Z]{6,30}$/g;
+    if(patron2.test(string)){
         return true;
-    }
-    else{
-        return false;
-    }
+     }
+     else{
+        return false; 
+     }
 }
 
-function validar_contrasena(stringA){
-    if(stringA.length>=6){
-        return true;
-    }
-    else{
-    return false;
-    }
-}
+function validar_contrasena (string){
+    // 6, minmo 6 sin maximo
 
-function confirmar_contrasena(stringA, stringB){
-    if(stringB.length>=6){
-        if(stringA==stringB){
-            return true;
+    patron1 = /^[A-Z0-9]{6,}$/g;
+ 
+
+        if(patron1.test(string)){
+           return true;
         }
         else{
+           return false; 
+        }
+        
+    } 
+
+function confirmar_contrasena(stringA, stringB){
+ if(validar_contrasena(stringA))
+    {
+        if(validar_contrasena(stringB))
+        {
+            if(stringA===stringB)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
             return false;
         }
     }
+
     else{
         return false;
     }
 }
 
+const stringprueba="AAAAA2";
+const stringprueba2="12345";
 
-module.exports.vali|dar_nombre_usuario = validar_nombre_usuario;
-module.exports.validar_contrasena=validar_contrasena;
-module.exports.confirmar_contrasena=confirmar_contrasena;
+const comprobarpassword = validar_contrasena (stringprueba);
 
-// module.exports = {
-//     validar_nombre_usuario,
-//     validar_contrasena,
-//     confirmar_contrasena
-// }
+console.log (comprobarpassword);
+
+// module.exports.validar_nombre_usuario = validar_nombre_usuario;
+// module.exports.validar_contrasena = validar_contrasena;
+// module.exports.confirmar_contrasena = confirmar_contrasena;
